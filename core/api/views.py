a70448aac8bf869a0c57aa4b3ac43fdde75ab5d2
roberto-fernandino <romfernandino@gmail.com>
2023-10-16 23:12:18 -0300

    functions to endpoints declared in urls.py

---
+++ core/api/views.py
@@ -0,0 +1,46 @@
+from urllib import response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .serializers import UsuarioSerializer
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+
+"""{
+    "cpf":"701.136.996-11",
+    "password": "dulce2014"
+}
+"""
+
+
+# Serializers here
+@api_view(["POST"])
+def login_usuario(request):
+    cpf = request.data.get("cpf")
+    password = request.data.get("password")
+    user = authenticate(cpf=cpf, password=password)
+    if user is not None:
+        if user.is_authenticated:
+            serialier = UsuarioSerializer(user)
+            return Response(serialier.data)
+        else:
+            return Response(
+                {
+                    "detail": "Credenciais Inválidas",
+                },
+                status=status.HTTP_404_NOT_FOUND,
+            )
+    else:
+        return Response(
+            {
+                "detail": "Credenciais Inválidas",
+            },
+            status=status.HTTP_404_NOT_FOUND,
+        )
+
+
+@api_view(["POST"])
+def postUsuario(request):
+    serializer = UsuarioSerializer(request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
