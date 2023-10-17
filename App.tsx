7440bad75c9845d6afd7c2d69de10a73bae79c62
roberto-fernandino <romfernandino@gmail.com>
2023-10-16 23:10:24 -0300

    Login page started integration with api.

--- App.tsx
+++ App.tsx
@@ -2,20 +2,39 @@ import React, {useState} from 'react';
 import {
   StyleSheet,
   View,
+  Text,
   Image,
   Dimensions,
   TextInput,
-  Button,
   ActivityIndicator,
+  TouchableOpacity,
 } from 'react-native';
 
 const LoginApp = () => {
   const [cpf, setCpf] = useState('');
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
+  const formatCpf = (inputCpf: string) => {
+    const numbers = inputCpf.replace(/\D/g, '');
+    const matches = numbers.match(/^(\d{0,3})(\d{0,3})?(\d{0,3})?(\d{0,2})?/);
+    if (matches) {
+      const mask = `${matches[1] || ''}${matches[2] ? '.' : ''}${
+        matches[2] || ''
+      }${matches[3] ? '.' : ''}${matches[3] || ''}${matches[4] ? '-' : ''}${
+        matches[4] || ''
+      }`;
+      return mask;
+    }
+    return '';
+  };
+
+  const HandleCpfChange = (text: string) => {
+    const formattedCpf = formatCpf(text);
+    setCpf(formattedCpf);
+  };
   const submitLogin = () => {
     setLoading(true);
-    fetch('https://minha-api.com/login', {
+    fetch('http://10.0.2.2:8000/usuarios/login/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -26,9 +45,14 @@ const LoginApp = () => {
       }),
     })
       .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.error('Error: ', error));
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.error('Error: ', error);
+      });
   };
+
   return (
     <View style={styles.container}>
       <Image style={styles.img} source={require('./img/gov.png')} />
@@ -37,18 +61,21 @@ const LoginApp = () => {
         placeholder="123.456.789-01"
         keyboardType="numeric"
         value={cpf}
-        onChangeText={setCpf}
+        onChangeText={HandleCpfChange}
       />
       <TextInput
         style={styles.input}
         placeholder="senha"
         value={password}
         onChangeText={setPassword}
+        secureTextEntry={true}
       />
       {loading ? (
         <ActivityIndicator size="large" color={'#ffffff'} />
       ) : (
-        <Button title="Login" onPress={submitLogin} />
+        <TouchableOpacity onPress={submitLogin} style={styles.submitButton}>
+          <Text style={styles.submitText}> Login </Text>
+        </TouchableOpacity>
       )}
     </View>
   );
@@ -68,8 +95,20 @@ const styles = StyleSheet.create({
   input: {
     padding: 10,
     height: 42,
+    width: 320,
     borderWidth: 1,
     margin: 12,
+  },
+  submitButton: {
+    width: 220,
+    backgroundColor: '#48ad44',
+    height: 42,
+    alignItems: 'center',
+    borderRadius: 10,
+  },
+  submitText: {
+    fontSize: 30,
+    color: '#ffff',
   },
 });
 
