cecc0d1091dc34013c6e8d148cda3cec9ca1097b
roberto-fernandino <romfernandino@gmail.com>
2023-10-17 22:40:16 -0300

    Added jazzmin admin panel to django.

--- core/core/settings.py
+++ core/core/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["10.0.2.2", "127.0.0.1"]
 # Application definition
 
 INSTALLED_APPS = [
+    "jazzmin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
