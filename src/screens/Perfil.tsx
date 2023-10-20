c7c560ef0dd37af50b88bccb3d6270d0409fc73a
roberto <roberto@pop-os.localdomain>
2023-10-20 08:24:22 -0300

    Modified perfil to accept params from the Home Screen correctly

--- src/screens/Perfil.tsx
+++ src/screens/Perfil.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
 export default function ProfileApp({route}) {
-  const {user_data} = route.params;
+  const {usuario_data} = route.params;
+  console.log('usuario_data: ', usuario_data);
   return (
     <View style={styles.informationContainer}>
       <View style={styles.informationCards}>
         <Text style={styles.cardHeader}>Informação Pessoal</Text>
-        <Text style={styles.cardInfo}>{user_data.nome}</Text>
-        <Text style={styles.cardInfo}>{user_data.cpf}</Text>
-        <Text style={styles.cardInfo}>{user_data.email}</Text>
+        <Text style={styles.cardInfo}>{usuario_data.nome}</Text>
+        <Text style={styles.cardInfo}>{usuario_data.cpf}</Text>
+        <Text style={styles.cardInfo}>{usuario_data.email}</Text>
+      </View>
+      <View style={styles.informationCards}>
+        <Text style={styles.cardHeader}>Endereço</Text>
+        <Text style={styles.cardInfo}>a</Text>
+        <Text style={styles.cardInfo}>b</Text>
+        <Text style={styles.cardInfo}>c</Text>
       </View>
     </View>
   );
@@ -24,15 +31,18 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    backgroundColor: '#grey',
+    backgroundColor: '#6f7471d5',
+    borderRadius: 10,
+    marginTop: 15,
   },
   cardHeader: {
     fontWeight: 'bold',
     fontSize: 50,
-    color: '##000000',
+    color: '#000',
   },
   cardInfo: {
     fontSize: 30,
-    color: '##000000',
+    color: '#000',
+    padding: 10,
   },
 });
