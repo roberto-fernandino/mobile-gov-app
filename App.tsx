7050c70f75f144c299ceaab70e350275b71da3b1
roberto <roberto@pop-os.localdomain>
2023-10-19 23:36:22 -0300

    Fixed app to work with navigation

--- App.tsx
+++ App.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
+import Routes from './src/routes';
 import {NavigationContainer} from '@react-navigation/native';
-import LoginApp from './components/loginApp';
-import HomeApp from './components/homeApp';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="loginApp" component={LoginApp} />
-        <Stack.Screen name="homeApp" component={HomeApp} />
-      </Stack.Navigator>
+      <Routes />
     </NavigationContainer>
   );
 }
