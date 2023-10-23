ed5150593a632b95ef71dcb305558f8ce5474ec0
roberto-fernandino <romfernandino@gmail.com>
2023-10-23 09:37:09 -0300

    New update on seralizers to ignore id

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -4,18 +4,30 @@ from usuarios.models import Usuario, Informações, Endereco
 
 class InformacoesSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = "__all__"
+        fields = [
+            "renda",
+            "numero_membros_familia",
+            "despesas_mensais",
+            "nivel_escolaridade",
+        ]
         model = Informações
 
 
 class EnderecoSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = "__all__"
+        fields = [
+            "cep",
+            "logradouro",
+            "numero",
+            "complemento",
+            "bairro",
+            "cidade",
+        ]
         model = Endereco
 
 
 # Serializers
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = "__all__"
+        fields = ["cpf", "nome", "sobrenome", "email", "data_nascimento"]
         model = Usuario
