21cb7f97f600885f370057c34ef99d551f2cd02e
roberto-fernandino <romfernandino@gmail.com>
2023-10-23 09:37:18 -0300

    new url for generate pdfs

--- core/api/urls.py
+++ core/api/urls.py
@@ -7,4 +7,5 @@ app_name = "api"
 urlpatterns = [
     path("login/", views.login_usuario, name="login_usuario"),
     path("signup/", views.add_user, name="singup_usuario"),
+    path("generate-pdf/<int:user_id>", views.generate_pdf, name="pdf-generate"),
 ]
